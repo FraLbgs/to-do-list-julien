874c27ccbf7fa3159a50989806b9f463bcfd28f6
Julien Richard <julien.richard.lh@gmail.com>
2022-07-23 11:39:38 +0200

    things to do: create a function without panic

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 
 /* things to do: put IDs */
 
+/* now, things to do: locate the selectors */
+
+const toDoButton = document.getElementById("to-do-button")
+const toDoInput = document.getElementById("to-do-input")
+const toDoList = document.getElementById("to-do-list")
+
+// console.log(todoInput, todoButton, todoList)
+
+/* now, add event listener */
+
+toDoButton.addEventListener("click", addThingsToDo)
+
+/* now, create a function without panic */
+
+function addThingsToDo(event) {
+    const taskContent = document.createElement("div");
+    taskContent.classList.add("to-do");
+
+    const newTask = document.createElement("li");
+    newTask.innerText = "hello world!";
+    newTask.classList.add("to-do-item")
+}
